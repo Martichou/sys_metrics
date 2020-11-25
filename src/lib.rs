2057f45ebeb72a04141aad5a5e23a98dd9e5c521
Martichou <martichou.andre@gmail.com>
2020-11-25 12:04:18 +0100

    Add Quick start in the lib.rs homepage

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,45 @@
 //! `sys_metrics` is a crate used to get a system's information.
+//!
+//! It attempt to provide information about:
+//!
+//!  * CPU
+//!  * Disks
+//!  * Host
+//!  * Memory
+//!  * Networks (wip)
+//!
+//! ## Quick start
+//! ```
+//! use sys_metrics::{cpu::*, disks::*, miscs::*, users::*};
+//! fn main() -> Result<(), std::io::Error> {
+//!     let host_info = match get_host_info() {
+//!         Ok(val) => val,
+//!         Err(x) => return Err(x),
+//!     };
+//! 
+//!     let _uuid = get_uuid().expect("Cannot retrieve UUID");
+//!     let _os = host_info.os_version;
+//!     let _hostname = host_info.hostname;
+//!     let _uptime = host_info.uptime;
+//!     let _cpufreq = match get_cpufreq() {
+//!         Ok(val) => val as i64,
+//!         Err(_) => -1,
+//!     };
+//!     let _loadavg = host_info.loadavg;
+//!     let _disks = match get_partitions_physical() {
+//!         Ok(val) => Some(val),
+//!         Err(_) => None,
+//!     };
+//!     let _iostats = match get_iostats() {
+//!         Ok(val) => Some(val),
+//!         Err(_) => None,
+//!     };
+//!     let _memory = host_info.memory;
+//!     let _users = get_users();
+//! 
+//!     Ok(())
+//! }
+//! ```
 
 /// Functions related to CPU stats
 pub mod cpu;
