4727a4ec210af6981755a618e3902e110c2afc66
Martichou <martichou.andre@gmail.com>
2020-11-26 17:33:19 +0100

    linux: Add logical cpu count

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,20 +2,11 @@ use sys_metrics::{cpu::*, disks::*, miscs::*, users::*};
 
 #[allow(unused_must_use)]
 fn main() {
-    let host_info = match get_host_info() {
-        Ok(val) => val,
-        Err(_) => return,
-    };
-    dbg!(host_info);
-
-    let uuid = get_uuid();
-    dbg!(uuid);
-    let cpu_freq = get_cpufreq();
-    dbg!(cpu_freq);
-    let disks = get_partitions_physical();
-    dbg!(disks);
-    let iostats = get_iostats();
-    dbg!(iostats);
-    let users = get_users();
-    dbg!(users);
+    dbg!(get_host_info());
+    dbg!(get_uuid());
+    dbg!(get_cpufreq());
+    dbg!(get_partitions_physical());
+    dbg!(get_iostats());
+    dbg!(get_users());
+    dbg!(get_cpu_logical_count());
 }
