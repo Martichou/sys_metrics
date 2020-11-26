e8e6dba4b228fffd17737b186122b2729f0565e7
Martichou <martichou.andre@gmail.com>
2020-11-26 17:40:49 +0100

    macOS: Add logical cpu count

--- src/cpu.rs
+++ src/cpu.rs
@@ -138,6 +138,36 @@ pub fn get_cpu_logical_count() -> Result<i64, Error> {
 }
 
 #[cfg(target_os = "macos")]
-pub fn get_cpu_logical_count() -> Result<u64, Error> {
-    todo!()
+pub fn get_cpu_logical_count() -> Result<i64, Error> {
+    let mut data: c_uint = 0;
+    let mib = [6, 25];
+
+    if unsafe {
+        sysctl(
+            &mib[0] as *const _ as *mut _,
+            mib.len() as u32,
+            &mut data as *mut _ as *mut c_void,
+            &mut std::mem::size_of::<c_uint>(),
+            std::ptr::null_mut(),
+            0,
+        )
+    } < 0
+    {
+        let mib = [6, 3];
+        if unsafe {
+            sysctl(
+                &mib[0] as *const _ as *mut _,
+                mib.len() as u32,
+                &mut data as *mut _ as *mut c_void,
+                &mut std::mem::size_of::<c_uint>(),
+                std::ptr::null_mut(),
+                0,
+            )
+        } < 0
+        {
+            return Err(Error::last_os_error());
+        }
+    }
+
+    Ok(data.into())
 }
