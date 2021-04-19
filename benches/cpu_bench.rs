f76256e71a07bc2894ced9e51ba274af82a491e0
Martichou <martichou.andre@gmail.com>
2021-04-19 14:04:16 +0200

    bench: add cpustat bench

--- benches/cpu_bench.rs
+++ benches/cpu_bench.rs
@@ -4,6 +4,7 @@ use sys_metrics::cpu::*;
 
 pub fn cpu_benches(c: &mut Criterion) {
     c.bench_function("get_cpufreq", |b| b.iter(|| get_cpufreq()));
+    c.bench_function("get_cpustat", |b| b.iter(|| get_cpustat()));
     c.bench_function("get_loadavg", |b| b.iter(|| get_loadavg()));
     c.bench_function("get_cpu_logical_count", |b| {
         b.iter(|| get_cpu_logical_count())
