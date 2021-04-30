40d9facea01f78ca52a4acb2021bc5664ba4e18b
Martichou <martichou.andre@gmail.com>
2021-04-30 23:12:43 +0200

    Add prototype for MacOS (iocounters) + bench

--- benches/network_bench.rs
+++ benches/network_bench.rs
@@ -1,7 +1,10 @@
 use criterion::Criterion;
 use criterion::{criterion_group, criterion_main};
+use sys_metrics::network::*;
 
-pub fn network_benches(_c: &mut Criterion) {}
+pub fn network_benches(c: &mut Criterion) {
+    c.bench_function("get_net_iocounters", |b| b.iter(|| get_net_iocounters()));
+}
 
 criterion_group!(benches, network_benches);
 criterion_main!(benches);
--- src/network/sys/macos/iocounters.rs
+++ src/network/sys/macos/iocounters.rs
@@ -1,1 +1,10 @@
+use crate::network::IoCounters;
 
+use std::io::{Error, ErrorKind};
+
+/// Return the [IoCounters] struct.
+///
+/// [IoCounters]: ../network/struct.IoCounters.html
+pub fn get_net_iocounters() -> Result<Vec<IoCounters>, Error> {
+    todo!()
+}
